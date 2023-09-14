08a2874c070b8d0fe65c4cbe001364e8e89441e1
Your Name <keazikun@bk.ru>
2023-09-14 23:14:02 +0300

    reshka

--- main.py
+++ main.py
@@ -1,35 +1,8 @@
-a = str(input("Начало игры в камень,ножницы,бумага.Первый игрок выбирает: "))
-b = str(input("Начало игры в камень,ножницы,бумага.Второй игрок выбирает: "))
-stone = "камень"
-paper = "бумага"
-scissors = "ножницы"
-
-first = "победил первый игрок"
-two = "победил второй игрок"
-draw = "ничья"
-
-if a == stone and b == paper:
-    print(first)
-elif a == stone and b == scissors:
-    print(two)
-
-elif a == paper and b == stone:
-    print(two)
-elif a == paper and b == scissors:
-    print(first)
-
-elif a == scissors and b == paper:
-    print(first)
-elif a == scissors and b == stone:
-    print(two)
 
 
-
-
-elif a == b:
-    print(draw)
-else:
-    print("Введите заново")
-
-
+reshka= input("Введите последовательность: ")
+r = 0
+while "р"*(r+1) in reshka:
+    r = +1
+print(r)
 
